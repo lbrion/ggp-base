2557d5c77c39aa76b14db08b5bbcace5956da677
lbrion <louisbrion@gmail.com>
2016-04-19 14:25:29 -0700

    Implemented minimax depth-limited

--- src/main/java/org/ggp/base/player/gamer/statemachine/sample/FaceHunter.java
+++ src/main/java/org/ggp/base/player/gamer/statemachine/sample/FaceHunter.java
@@ -1,5 +1,8 @@
+//Currently implements Depth-Limited Search with minimax
+
 package org.ggp.base.player.gamer.statemachine.sample;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -14,37 +17,107 @@ import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
 
 public class FaceHunter extends SampleGamer {
 	private Random r = new Random();
+	private int limit = 3; //Arbitrary int depth limit
 
 	@Override
 	public String getName() {
-        return "";
+        return "Face Hunter";
     }
 
 	@Override
 	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
     {
-		StateMachine thisMachine = getStateMachine();
+		System.out.println("Starting");
+		StateMachine game = getStateMachine();
         long start = System.currentTimeMillis();
         long finishBy = timeout - 1000;
 
-        List<Move> actions = thisMachine.findLegals(getRole(), getCurrentState());
+        List<Move> actions = game.findLegals(getRole(), getCurrentState());
         Move selection = (actions.get(r.nextInt(actions.size())));
 
         int score = 0;
 
         for(int i = 0; i < actions.size(); i++) {
-        	int result = minscore(getRole(), actions.get(i), getCurrentState());
-        }
+        	//if (System.currentTimeMillis() > finishBy) break;
 
+        	int result = minscore(getRole(), actions.get(i), getCurrentState(), 0);
+        	if(result > score) {
+        		score = result;
+        		selection = actions.get(i);
+        	}
+        }
+        System.out.println(score);
         long stop = System.currentTimeMillis();
         notifyObservers(new GamerSelectedMoveEvent(actions, selection, stop - start));
 		return selection;
     }
 
-	public int minscore(Role role, Move action, MachineState state) {
+	public int maxscore(Role role, MachineState state, int level) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException
+	{
+		System.out.println("Maxing");
+		StateMachine game = getStateMachine();
 
+		if(game.isTerminal(state)) {
+			return game.findReward(role, state);
+		}
 
-		return 0;
+		if(level >= limit) return 0; //This is where you would return eval function
+
+		int score = 0;
+
+		List<Move> actions = game.findLegals(role, state);
+
+		for(int i = 0; i < actions.size(); i++) {
+			int result = minscore(role, actions.get(i), state, level);
+			if(result == 100) {
+				return 100;
+			}
+			if(result > score) {
+				score = result;
+			}
+		}
+
+		return score;
+	}
+
+	public int minscore(Role role, Move action, MachineState state, int level) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException
+	{
+		System.out.println("Mining");
+
+		StateMachine game = getStateMachine();
+
+		Role opponent = null;
+		//Get opponent role (only works in dual player)
+		for(Role possible: game.findRoles()) {
+			if(!possible.equals(getRole())) {
+				opponent = possible;
+			}
+		}
+
+		List<Move> actions = game.findLegals(opponent, state);
+
+		int score = 100;
+
+		for(int i = 0; i < actions.size(); i++) {
+			List<Move> move = new ArrayList<Move>();
+			if(role.equals(game.getRoles().get(0))) {
+				move.add(action);
+				move.add(actions.get(i));
+			}
+			else {
+				move.add(actions.get(i));
+				move.add(action);
+			}
+
+			MachineState nextState = game.findNext(move, state);
+
+			int result = maxscore(role, nextState, level + 1);
+
+			if(result == 0) return 0;
+			if(result < score) score = result;
+		}
+
+		return score;
 	}
 
 	@Override
